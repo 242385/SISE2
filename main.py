1dc8aa9a967d4bce973c1ec5bc7ab9fc458bcc90
bkalach <242414@edu.p.lodz.pl>
2023-05-29 21:53:37 +0200

    added serialization to files using pickle rick

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 import re
 import queue
 import time
-
+import pickle
 import numpy as np
 
 from classes.neuron import *
@@ -224,6 +224,16 @@ def test(mlp, test_inputs, test_targets):
 
 train(mlp, dataVector, target, 0.5, 0.9, 100)
 test(mlp, testedData, target)
+
+##ZAPISYWANIE
+file = open('networks/network.txt', 'wb')
+pickle.dump(mlp, file)
+file.close()
+
+##ODCZYTYWANIE
+file = open('networks/network.txt', 'rb')
+mlpOdczytaneZPliku = pickle.load(file)
+file.close()
 
 if programMode == 0:
     import learning
